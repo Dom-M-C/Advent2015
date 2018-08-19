1c67b5bb5e4330c343363f61920c8fc404fb546a
Dmc <shen.nung@gmail.com>
2018-08-19 12:11:14 +0100

    shelving this attempt at problem 3

--- src/Advent2015_3.hs
+++ src/Advent2015_3.hs
@@ -5,7 +5,23 @@ import Data.Maybe
 import Data.HashMap.Lazy (HashMap)
 import qualified Data.HashMap.Lazy as Map
 
-data Direction = None | North | East | South | West deriving(Show)
+data State s a = State { exec :: s -> (a, s) }
+
+
+data Direction = None | North | East | South | West deriving(Show, Enum)
+
+--instance Ord Direction
+
+--instance State Direction where
+--    exec d
+
+mapDirectionToMove :: Direction -> Key
+mapDirectionToMove d = case d of
+    None -> (0, 0)
+    North -> (0, 1)
+    East -> (1, 0)
+    South -> (0, -1)
+    West -> (-1, 0)
 
 data Move
     = Reset { initial :: Key } --Initial move, resets visits
@@ -25,7 +41,6 @@ updateVisits :: Key -> Grid -> Grid
 --updateVisits = undefined
 updateVisits key grid = Map.insertWith (\x y -> (y+1)) key v grid
     where   lkv = Map.lookup key grid
-            --v = maybe 0 (x -> x) lkv
             v = fromMaybe 0 lkv
 
 
@@ -33,13 +48,6 @@ updateVisits key grid = Map.insertWith (\x y -> (y+1)) key v grid
 move (Reset _) = Reset { initial = (0, 0) }
 move m = undefined
 
-mapDirectionToMove :: Direction -> Key
-mapDirectionToMove d = case d of
-    None -> (0, 0)
-    North -> (0, 1)
-    East -> (1, 0)
-    South -> (0, -1)
-    West -> (-1, 0)
 
 moveLoop :: [Direction] -> [Key]
 moveLoop [] = []
